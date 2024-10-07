54d78f0fea1494806a4be914fe7e7e8c4dc0c4c1
Jamena McInteer <jamena@meshify.com>
2024-10-07 10:35:27 -0400

    Fix: relative path to assets folder (docs)

--- docs_src/vite.config.ts
+++ docs_src/vite.config.ts
@@ -4,8 +4,10 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
   plugins: [react(), mkcert()],
   build: {
     outDir: "../docs",
+    assetsDir: "./assets",
   },
 });
