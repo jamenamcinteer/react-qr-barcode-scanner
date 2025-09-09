5aad85a98843d6975d0bfd2778aa3e1cef8c7c49
Jamena McInteer <jamena@meshify.com>
2025-09-09 10:01:45 -0400

    Fix tsx import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import BarcodeScanner from "./BarcodeScanner.ts";
+import BarcodeScanner from "./BarcodeScanner.tsx";
 export { default as BarcodeStringFormat } from "./BarcodeStringFormat.ts";
 export { BarcodeFormat } from "@zxing/library";
 export default BarcodeScanner;
