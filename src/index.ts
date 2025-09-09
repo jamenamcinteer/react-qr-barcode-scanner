48970091d2ca0d9b16298c4c97cf181f8ec31aa2
Jamena McInteer <jamena@meshify.com>
2025-09-09 09:58:46 -0400

    Add .js extensions to imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import BarcodeScanner from "./BarcodeScanner";
-export { default as BarcodeStringFormat } from "./BarcodeStringFormat";
+import BarcodeScanner from "./BarcodeScanner.ts";
+export { default as BarcodeStringFormat } from "./BarcodeStringFormat.ts";
 export { BarcodeFormat } from "@zxing/library";
 export default BarcodeScanner;
